b353634447b9f6204e2049a5a8d0a9075e9f25ef
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-16 21:55:15 +0600

    state done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1>Incrase Decrease function</h1>
-    
+      <Counter></Counter>
     </div>
   );
+}
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  return (
+    <div className="">
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Increase</button>
+    </div>
+  )
 }
 
 export default App;
