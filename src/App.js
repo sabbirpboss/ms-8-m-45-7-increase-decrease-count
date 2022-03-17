3205f8551978c6e4475e2f9428aab3cb6d5e6980
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-17 12:11:54 +0600

    Load dynamic data, API call useEffect integrate state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>Incrase Decrease function</h1>
-      {/* <Counter></Counter> */}
+      {/* <h1>Incrase Decrease function</h1>
+      <Counter></Counter> */}
+
+      <h2>useEffect</h2>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
 //useEffect
 const ExternalUsers = () => {
+  const [users, setUsers] = useState([]);
+  // useEffect( () => {}, [])
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
   return (
     <div className="">
       <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name = {user.name} email = {user.email} phone = {user.phone}></User>)
+      }
+    </div>
+  )
+}
+
+const User = (props) => {
+  return (
+    <div className="container">
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.email}</p>
+      <p><small>Phone: {props.phone}</small></p>
     </div>
   )
 }
